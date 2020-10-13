25ae6537ac1325dcd47e22630685702b0fd94f10
Hernan Diaz <hernandiaz035@gmail.com>
2020-10-13 16:12:17 +1100

    Add Profile inline menu to Users in Admin site.

--- users/admin.py
+++ users/admin.py
@@ -2,9 +2,25 @@
 
 # Django
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from users.models import Profile
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     pass
+
+
+class ProfileInline(admin.StackedInline):
+    """Profile InLine Admin Form"""
+    model = Profile
+    verbose_name_plural = 'Profile'
+    can_delete = False
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
