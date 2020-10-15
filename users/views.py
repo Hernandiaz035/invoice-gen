a7a65e788555f2821d7013122e1b63f62086752b
Hernan Diaz <hernandiaz035@gmail.com>
2020-10-15 23:20:54 +1100

    Change succes URL to lazy reverse.

--- users/views.py
+++ users/views.py
@@ -3,12 +3,13 @@
 # Django
 from django.contrib.auth import views as auth_views
 from django.shortcuts import render
+from django.urls.base import reverse_lazy
 
 
 class LoginView(auth_views.LoginView):
     template_name = 'users/login.html'
     redirect_authenticated_user = True
-    success_url = 'users/dashboard.html'
+    success_url = reverse_lazy('users:dashboard')
 
 
 def dashboardView(request):
